882f9c589e299ca65d795edba1e059c9b07c0ccc
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-27 09:46:33 +0530

    Update CityJpaRepository.java

--- src/main/java/com/example/geohub/repository/CityJpaRepository.java
+++ src/main/java/com/example/geohub/repository/CityJpaRepository.java
@@ -8,12 +8,13 @@
  */
 
 // Write your code here
-package com.exapmle.geohub.repository;
+package com.example.geohub.repository;
+
 import com.example.geohub.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityJpaRepository extends JpaRepository<City, Integer> {
-    
+
 }
