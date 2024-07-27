76dfd6683fe91bc72dff7c2a2db3150667a7bc28
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-27 09:47:17 +0530

    Update CountryJpaRepository.java

--- src/main/java/com/example/geohub/repository/CountryJpaRepository.java
+++ src/main/java/com/example/geohub/repository/CountryJpaRepository.java
@@ -9,10 +9,12 @@
 
 // Write your code here
 package com.example.geohub.repository;
+
 import com.example.geohub.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
 public interface CountryJpaRepository extends JpaRepository<Country, Integer> {
-    
+
 }
